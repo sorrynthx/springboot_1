fe0e66f2989cab5329cb02e8b20c27e6f1ae53c3
sorrynthx <sorrynthx@gmail.com>
2022-03-10 12:21:18 +0900

    2022-03-10 AWS-springboot(이동욱) 프로젝트 생성

---
+++ freelec/src/main/java/com/freelec/spring/FreelecApplication.java
@@ -0,0 +1,13 @@
+package com.freelec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecApplication.class, args);
+	}
+
+}
---
+++ freelec/src/test/java/com/freelec/spring/FreelecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelec.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
