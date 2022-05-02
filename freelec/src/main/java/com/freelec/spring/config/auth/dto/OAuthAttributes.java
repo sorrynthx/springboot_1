f6bcd44cbf7e66c8a7e0327c0d6eae214cbf98ae
sorrynthx <sorrynthx@gmail.com>
2022-05-02 16:45:59 +0900

    2022-05-02 AWS-springboot(이동욱) 네아버 로그인 추가

--- freelec/src/main/java/com/freelec/spring/config/auth/dto/OAuthAttributes.java
+++ freelec/src/main/java/com/freelec/spring/config/auth/dto/OAuthAttributes.java
@@ -32,6 +32,11 @@ public class OAuthAttributes {
 	public static OAuthAttributes of(String registrationId,
 									 String userNameAttributeName,
 									 Map<String, Object> attributes) {
+		
+		if ("naver".equals(registrationId)) {
+			return ofNaver("id", attributes);
+		}
+		
 		return ofGoogle(userNameAttributeName, attributes);
 	}
 
@@ -47,6 +52,21 @@ public class OAuthAttributes {
 								.nameAttributeKey(userNameAttributeName)
 								.build();
 	}
+	
+	private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+		
+		Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+		
+		return OAuthAttributes.builder()
+				                .name((String) response.get("name"))
+				                .email((String) response.get("email"))
+				                .picture((String) response.get("profile_image"))
+				                .attributes(response)
+				                .nameAttributeKey(userNameAttributeName)
+				                .build();
+		        
+	}
+	
 	
 	// User 엔티티 생성. OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입할 때. 가입 시, 기본 권한을 GUEST로 사용
 	public User_ toEntity() {
